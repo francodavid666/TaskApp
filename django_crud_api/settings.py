211fda635f58117c8280ef4bc2e20d4506b30ed1
franco66 <david_franco@outlook.com>
2023-09-18 12:51:15 -0300

    0f0

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -82,15 +82,9 @@ WSGI_APPLICATION = "django_crud_api.wsgi.application"
 # }
 
 DATABASES = {
-    "default":{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'railway',
-        'USER':'postgres',
-        'PASSWORD':'HXJMKqdiaQEQQ8Lofn9R',
-        'HOST':'containers-us-west-183.railway.app',
-        'PORT':'5498',
-    }
- 
+    "default":
+    dj_database_url.config(default="sqlite:///" +
+                           os.path.join(BASE_DIR, "db.sqlite3"))
 }
 
 # Password validation
