ac6b58640881899e68b8a61d351d0d70c5bb72c7
franco66 <david_franco@outlook.com>
2023-09-18 13:41:52 -0300

    fff

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "django_crud_api.wsgi.application"
 
 # Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databasess  s
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databasess s s
 
 # DATABASES = {
 #     "default": {
