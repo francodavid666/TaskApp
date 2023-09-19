06d0bf25f8a58b7ff292b73ed1e2023b2b4f81f4
franco66 <david_franco@outlook.com>
2023-09-19 18:18:34 -0300

    a

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -11,7 +11,7 @@ export const getAllTasks = () => tasksApi.get("/");
 
 export const getTask = (id) => tasksApi.get(`/${id}`);
 
-export const createTask = (task) => tasksApi.post("/", task);
+export const createTask = (task) => tasksApi.post(`/${task}`);
 
 export const updateTask = (id, task) => tasksApi.put(`/${id}/`, task);
 
