64a38267ad90be1917ca7eca6304038f745752fc
franco66 <david_franco@outlook.com>
2023-09-19 18:46:09 -0300

    as

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -4,14 +4,14 @@ import axios from "axios";
 
 console.log(URL);
 const tasksApi = axios.create({
-  baseURL: `https://tasksapi.up.railway.app/tasks/api/v1/tasks`,
+  baseURL: `https://tasksapi.up.railway.app/tasks/api/v1/tasks/`,
 });
 
 export const getAllTasks = () => tasksApi.get("/");
 
 export const getTask = (id) => tasksApi.get(`/${id}`);
 
-export const createTask = (task) => tasksApi.post(`/${task}`);
+export const createTask = (task) => tasksApi.post("/", task);
 
 export const updateTask = (id, task) => tasksApi.put(`/${id}/`, task);
 
