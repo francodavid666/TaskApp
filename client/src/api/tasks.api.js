9351533dcbd48acf9c752035bb71e5a87acb180d
franco66 <david_franco@outlook.com>
2023-09-19 21:58:04 -0300

    df

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -5,10 +5,11 @@ const URL = process.env.NODE_ENV === 'production' ?
 import.meta.env.VITE_BACKEND_URL :'http://127.0.0.1:8000' 
 
 
-console.log(URL);
-const tasksApi = axios.create({
-  baseURL: `https://tasksapi.up.railway.app/tasks/api/v1/tasks/`,
-});
+const tasksApi = axios.create(
+  {
+     baseURL: `${URL}/tasks/api/v1/tasks`
+  }
+)
 
 export const getAllTasks = () => tasksApi.get("/");
 
