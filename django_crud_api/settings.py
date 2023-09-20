c039426a1838271eacd0035304d6d91820b3b68c
franco66 <david_franco@outlook.com>
2023-09-19 21:55:21 -0300

    env

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import dj_database_url
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir1'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
