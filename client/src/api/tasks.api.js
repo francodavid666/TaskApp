430311c08d08f600e758b1ab44432e26d96c1151
franco66 <david_franco@outlook.com>
2023-09-19 21:45:38 -0300

    dd

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 
+const URL = process.env.NODE_ENV === 'production' ?  
+import.meta.env.VITE_BACKEND_URL :'http://127.0.0.1:8000' 
+
 
 console.log(URL);
 const tasksApi = axios.create({
--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -82,16 +82,11 @@ WSGI_APPLICATION = "django_crud_api.wsgi.application"
 # }
 
 DATABASES = {
-    "default":{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'railway',
-        'USER':'postgres',
-        'PASSWORD':'HXJMKqdiaQEQQ8Lofn9R',
-        'HOST':'containers-us-west-183.railway.app',
-        'PORT':'5498',
-    }
- 
+    "default":
+    dj_database_url.config(default="sqlite:///" +
+                           os.path.join(BASE_DIR, "db.sqlite3"))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
